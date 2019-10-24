9a520970784025e885782078c1e53a3bc7d8d6e7
Jérémy Masson <jeremy.masson@wildcodeschool.fr>
2019-10-24 17:47:31 +0200

    Fixed Drug entity without accessors.

--- src/main/java/fr/elvynia/formation/doctor/entity/Drug.java
+++ src/main/java/fr/elvynia/formation/doctor/entity/Drug.java
@@ -34,4 +34,37 @@ public class Drug implements Serializable {
 			joinColumns = @JoinColumn(name = "CONSULTATION_id"),
 			inverseJoinColumns = @JoinColumn(name = "DRUG_id"))
 	private List<Consultation> consultations;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Consultation> getConsultations() {
+		return consultations;
+	}
+
+	public void setConsultations(List<Consultation> consultations) {
+		this.consultations = consultations;
+	}
+
 }
